ff8b3cd70689e994a64c63c60b1494d30a387714
btrollo <btrollo@stevens.edu>
2019-05-12 14:35:45 -0400

    Fixed href to homepage

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -20,7 +20,7 @@ function App(isLoggedIn) {
         <header className="App-header">
           <Navbar bg="dark" variant="dark">
             <Container fluid={true}>
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="/">
             <img
             src="/blog.png"
             width="50"
@@ -29,14 +29,6 @@ function App(isLoggedIn) {
             />
     </Navbar.Brand>
               <Nav className="mr-auto">
-               
-                <NavDropdown title="Articles" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
                 {isLoggedIn.isLoggedIn ? <LogOut /> : <AccountButtons isLoggedIn={isLoggedIn.isLoggedIn} />}
               </Nav>
             </Container>
