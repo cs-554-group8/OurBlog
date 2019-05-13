10706070b7a6fa0d23ef58fd2ef9e5ba3c69b286
Sanchita Rastogi <sanchitarastogi03@gmail.com>
2019-05-13 07:11:34 -0400

    alignment and css

--- code/client/src/ShowProfile.js
+++ code/client/src/ShowProfile.js
@@ -1,95 +1,108 @@
 import React, { Component } from "react";
 import queries from './queries';
 import { Query } from 'react-apollo';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
 import UpdateUser from './components/UpdateUser'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 class ShowProfile extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        address: "",
-        name: "",
-        interest: "",
-        complete: false
-      };
-       
+        super(props);
+        this.state = {
+            address: "",
+            name: "",
+            interest: "",
+            complete: false
+        };
+
     }
-  
-     
-      render(){
-            return (<div>
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col lg={8}>
-                        <Query query = {queries.ME}>
-                        {({ data }) => {
-                                    if (!data) {
-                                        return (
-                                            <div>
-                                            </div>
-                                        );
-                                    }
-                                    const { me } = data;
-                                    console.log("me data:" , data)
-                                    if (!me) {
-                                        return (
-                                            <div>
-                                            </div>
-                                        );
-                                    }
-                                   
-                                    else{
-                                        console.log("data in blog", data);
-                                        return(
-                                            <div>
-                                               <p>
-                                                   Name: {me.name}
-                                                   <br/>
-                
-                                                   Email: {me.email}
-                                                   <br/>
-                                                   Phone: {me.phone}
-                                                   <br/>
 
-                                                    Interest: {me.interest}
-                                                    <br/>
 
-                                                Address: {me.address}
-                                                <br/>
-                                                Blogs:   {me.blogs.map((blog, index) => (
-                                                            <p> 
-                                                                {blog.title}
-                                                            </p>
-                                                            ))}
-                                               
-                                                Comments: {me.comments.map((comment, index) => (
-                                                            <p> 
-                                                                {comment.content}
-                                                                {comment.forBlog ? comment.forBlog.title : <p>Blog not found</p>}
-                                                            </p>
-                                                            ))}
+    render() {
+        return (<div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col lg={8}>
+                        <Query query={queries.ME}>
+                            {({ data }) => {
+                                if (!data) {
+                                    return (
+                                        <div>
+                                        </div>
+                                    );
+                                }
+                                const { me } = data;
+                                console.log("me data:", data)
+                                if (!me) {
+                                    return (
+                                        <div>
+                                        </div>
+                                    );
+                                }
 
-</p>
-</div>
-                                                
-                                               
-                                        )}
-                        }}
+                                else {
+                                    return (
+                                        <div>
+                                            <ListGroup className="justify-content-md-center">
+                                                <ListGroup.Item >
+                                                    <b>Name</b>: {me.name}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+                                                    <b>Email</b>: {me.email}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+                                                    <b>Phone</b>: {me.phone}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+                                                    <b>Interest</b>: {me.interest}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+                                                    <b>Address</b>: {me.address}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+                                                    <b>Blogs</b>: {me.blogs.map((blog, index) => (
+                                                        <ul> {blog.title}</ul>
+                                                    ))}
+                                                </ListGroup.Item>
+
+                                                <ListGroup.Item>
+
+                                                    <b> Comments</b>: {me.comments.map((comment, index) => (
+                                                        <Card.Header> <b>Blog:</b> {comment.forBlog ? comment.forBlog.title : <p>Blog not found</p>}
+                                                            <br />
+                                                            <ListGroup.Item>  {comment.content} </ListGroup.Item>
+
+                                                        </Card.Header>
+                                                    ))}
+
+                                                </ListGroup.Item>
+
+
+                                            </ListGroup>
+                                        </div>
+
+
+                                    )
+                                }
+                            }}
 
                         </Query>
-                        </Col>
-                    </Row>
-                    <button >
-                    <Link to = '/profile/update'>
+                    </Col>
+                </Row>
+                <Button variant='outline-primary'>
+                    <Link to='/profile/update'>
                         Edit Profile
                      </Link>
-                    </button>
-                  
-                </Container>
-            </div>)
-}
+                </Button>
+
+            </Container>
+        </div>)
+    }
 }
 
-      
-  export default ShowProfile
+
+export default ShowProfile
